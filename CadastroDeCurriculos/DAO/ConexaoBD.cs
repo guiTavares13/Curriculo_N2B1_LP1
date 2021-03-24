2ea9763b14ee07fa3cf05859fb05bda161da4075
Guilherme Tavares <guiftt035@outlook.com>
2021-03-24 20:21:12 -0300

    Add project files.

---
+++ CadastroDeCurriculos/DAO/ConexaoBD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDeCurriculos.DAO
+{
+    public static class ConexaoBD
+    {
+        public static SqlConnection GetConexao()
+        {
+            string strCon = "Data Source=DESKTOP-6TEUHKL\\SQLEXPRESS; Database=N2_1B; integrated security=true";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
